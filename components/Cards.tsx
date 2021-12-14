22c64d3534f89404b0b15e621ac9f44c2d928dc5
RichardKentos <77812166+RichardKentos@users.noreply.github.com>
2021-12-14 11:08:45 +0100

    font points

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -12,7 +12,7 @@ const [ points, setPoints ] = useState(0)
         <Card points={points} handlePoints={setPoints} handleClick={setCurrentQuestion} question={questions[currentQuestion]}/>
         ) :
          (<>
-            <h1 className="text-center w-full font-semibold text-5xl my-4">Dosiahli ste <span className="text-red-500 font-bold text-9xl">{points} bodov</span></h1>
+            <h1 className="text-center w-full font-semibold text-5xl my-4">Dosiahli ste <span className="text-red-500 font-bold text-7xl">{points} bodov</span></h1>
          </>)
          }
           
