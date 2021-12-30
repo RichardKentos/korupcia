e20c43c7e876a9aa2f247cd9860df9c7bb9bcd25
RichardKentos <77812166+RichardKentos@users.noreply.github.com>
2021-12-30 21:51:37 +0100

    points fix

--- components/Results.tsx
+++ components/Results.tsx
@@ -3,19 +3,17 @@ import { useEffect, useState } from "react";
 
 const Results = ({ points, setCurrentQuestion, setPoints,explanation,wrong, setWrong } : any ) => {
     
-console.log(explanation[2]);
-
     return (
         <div className="bg-gray-100 rounded-md md:w-2/3 mx-auto h-min shadow-md my-8 p-4 flex flex-wrap justify-evenly items-center ">
-            <h1 className="text-center w-full font-semibold text-5xl my-4">Dosiahli ste <span className="block"><span className={` ${points>10 && "text-green-500"}  ${points<11 && "text-yellow-500"}  ${points<6 && "text-red-500"} font-bold text-7xl`} >{points}</span> / 15 bodov</span></h1>
+            <h1 className="text-center w-full font-semibold text-5xl my-4">Dosiahli ste <span className="block"><span className={` ${points > 7 && "text-green-500"}  ${points < 8 && "text-yellow-500"}  ${points < 4 && "text-red-500"} font-bold text-7xl`} >{points}</span> / 15 bodov</span></h1>
             <div className="w-full">
-            {points > 10 ? (<div className="w-full flex flex-wrap justify-evenly">
+            {points > 7 ? (<div className="w-full flex flex-wrap justify-evenly">
                 <img className="md:w-1/4 md:block hidden w-2/3 mt-8 opacity-20"  src="/bad.svg"></img>
                 <img className="md:w-1/4 md:block hidden w-2/3 mt-8 opacity-20"  src="/medium.svg"></img>
                 <img className="md:w-1/4 md:block w-2/3 mt-8"  src="/good.svg"></img>
                 <h3 className="w-full block text-center text-3xl font-bold">Protikorupčný bojovník</h3>
                 <p>Chválime ťa, zvládol si náš test s veľmi dobrými výsledkami.</p>
-            </div>) : (points < 6 ? (<div className="w-full flex flex-wrap justify-evenly">
+            </div>) : (points < 4 ? (<div className="w-full flex flex-wrap justify-evenly">
                 <img className="md:w-1/4 md:block w-2/3 mt-8"  src="/bad.svg"></img>
                 <img className="md:w-1/4 md:block hidden w-2/3 mt-8 opacity-20"  src="/medium.svg"></img>
                 <img className="md:w-1/4 md:block hidden w-2/3 mt-8 opacity-20"  src="/good.svg"></img>
--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,7 @@ const Home: NextPage = ( {questions}: any) => {
 }
 
 export async function getStaticProps() {
-  const response = await fetch("https://jsonkeeper.com/b/F077");
+  const response = await fetch("https://jsonkeeper.com/b/9ZOY");
   const questions = await response.json();
   return {
       props: {
